436201e65290f842630957a5332958b4c081ffe3
mayank0202 <mayank.sharma@sourcefuse.com>
2023-10-17 20:54:28 +0530

    updated vpc-subnets and workflows

--- main.tf
+++ main.tf
@@ -22,23 +22,22 @@ data "aws_vpc" "vpc" {
   }
 }
 
-## network
-data "aws_subnets" "public" {
-  filter {
-    name = "tag:Name"
-    values = [
-      "${var.namespace}-${var.environment}-public-subnet-public-${var.region}a",
-      "${var.namespace}-${var.environment}-public-subnet-public-${var.region}b"
-    ]
-  }
-}
-
 data "aws_subnets" "private" {
   filter {
     name = "tag:Name"
     values = [
-      "${var.namespace}-${var.environment}-private-subnet-private-${var.region}a",
-      "${var.namespace}-${var.environment}-private-subnet-private-${var.region}b"
+      "${var.namespace}-${var.environment}-private-${var.region}a",
+      "${var.namespace}-${var.environment}-private-${var.region}b",
+    ]
+  }
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name = "tag:Name"
+    values = [
+      "${var.namespace}-${var.environment}-public-${var.region}a",
+      "${var.namespace}-${var.environment}-public-${var.region}b",
     ]
   }
 }
